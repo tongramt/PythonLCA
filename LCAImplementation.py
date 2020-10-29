9b88234662ffb9c23979f5c37bcde956278ab749
tongramt <grantth@tcd.ie>
2020-10-29 17:13:17 +0000

    Unit Testing Added to Python Implementation

---
+++ LCAImplementation.py
@@ -0,0 +1,31 @@
+# n1 and n2
+
+# A Binary tree node
+class Node:
+
+    # Constructor to create a new node
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+# Function to find LCA of n1 and n2. The function assumes
+# that both n1 and n2 are present in BST
+def findLCA(root, n1, n2):
+
+    # Base Case
+    if root is None:
+        return None
+
+    # If both n1 and n2 are smaller than root, then LCA
+    # lies in left
+    if(root.data > n1 and root.data > n2):
+        return lca(root.left, n1, n2)
+
+    # If both n1 and n2 are greater than root, then LCA
+    # lies in right
+    if(root.data < n1 and root.data < n2):
+        return lca(root.right, n1, n2)
+
+    return root
+
